b20cfdaf5b08f2845cdb0d645e786a36f4fc9f85
Bhumika2603 <99654729+Bhumika2603@users.noreply.github.com>
2024-01-29 17:16:10 +0530

    commit

---
+++ evaluate.py
@@ -0,0 +1,30 @@
+from preprocessing import Preprocessing
+from train import Train_model
+import sys
+from model import load_model
+import pickle
+
+with open('encoder.pkl','rb') as enco:
+    encoder=pickle.load(enco)
+
+with open('model.pkl', 'rb') as fp:
+    dict=pickle.load(fp)
+
+task="task1"
+preprocess = Preprocessing() 
+df,cat_cols= preprocess.preprocess_ev_data(r"C:\Users\hp\Downloads\p.xlsx",dict["mean_value"],task)
+df=preprocess.encode_ev(df,cat_cols,encoder)
+  
+df=preprocess.scale_ev_data(df)
+
+train = Train_model()
+classifier = dict['model']
+y_pred_proba_test = train.evaluate(classifier,df)
+y_predict=train.getPredUsingOptimalThrehold(dict['threshold'],y_pred_proba_test)
+print(y_predict)
+
+
+
+
+
+
